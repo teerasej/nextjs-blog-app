eff4ae65a6c3471137dd2f5098af85b11e5b9755
Teerasej Jiraphatchandej <teerasej@gmail.com>
2022-02-26 18:00:06 +0700

    create component to be used as page's template

--- components/header.js
+++ components/header.js
@@ -1,3 +1,4 @@
+import Link from "next/link"
 
 export default function Header({hero}) {
     hero = '/images/' + (hero || 'orb.jpg')
--- components/layouts.js
+++ components/layouts.js
@@ -1,11 +1,13 @@
+import Footer from "./footer";
 import Header from "./header";
 
 
-export default function Layout({children, hero}) => {
+export default function Layout({children, hero}) {
     return (
         <>
             <Header hero={hero}/>
             <main>{children}</main>
+            <Footer/>
         </>
     )
 }
