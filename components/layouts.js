349df799fa5c550e4917fe55470de42ce0a982c3
Teerasej Jiraphatchandej <teerasej@gmail.com>
2022-02-26 16:04:31 +0700

    add header to layouts

---
+++ components/layouts.js
@@ -0,0 +1,11 @@
+import Header from "./header";
+
+
+export default function Layout({children, hero}) => {
+    return (
+        <>
+            <Header hero={hero}/>
+            <main>{children}</main>
+        </>
+    )
+}
